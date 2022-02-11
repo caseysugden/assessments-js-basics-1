2d8a538df91cccb642843bfc7bc6f6868e7f30d9
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-11 09:57:50 -0700

    Finished Problem 1 and Problem 2

--- assessment-js-basics-1/sldc.js
+++ assessment-js-basics-1/sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is the second step, in which one determines what the software is supposed to do, and what it required achieve that goal. Also, this includes defining what resources are required."
+const design = "This is the third step, in which one models the way a software application will work. It may include incorporating prototypes and using high-level code."
+const implementation = "This is the fourth step, in which one actually writes the program. Most likely done by a team for larger projects, software engineers will work with managers, designers, and architects to implement the code."
+const testingAndIntegration = "This is the fifth step, in which the software is tested before deployment. This is critical to make sure the code is functioning and secure. This helps to reduce bugs and errors."
+const maintain = "This is the final step, which includes updated your software as things change."
